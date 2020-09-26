3c2470dcecb03972a54332c2368a242e07de468d
JUAN DANIEL TREJOS PEREZ <jtrejosp@est.utn.ac.cr>
2020-09-26 17:11:27 -0600

    Add class person

---
+++ MatriculaUniversitaria/MatriculaUniversitaria/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Person
+    {
+        public int id { get; set; }
+        public int dni { get; set; }
+        public string name { get; set; }
+        public string last { get; set; }
+        public char sex { get; set; }
+        public DateTime bornDate { get; set; }
+        public string academyLvl { get; set; }
+        public DateTime admisionDate { get; set; }
+        public string admisionUser { get; set; }
+        public string nationality { get; set; }
+        public string state { get; set; }
+
+        public string printPerson() 
+        {
+            return this.id + "-"+this.dni + "-" + this.name + "-"+this.last+"-"+this.sex+"-"+this.bornDate
+                +"-"+this.admisionDate+"-"+this.admisionUser+"-"+this.nationality+"-"+this.state;
+        }
+
+    }
+}
--- MatriculaUniversitaria/MatriculaUniversitaria/Program.cs
+++ MatriculaUniversitaria/MatriculaUniversitaria/Program.cs
@@ -14,9 +14,7 @@ namespace MatriculaUniversitaria
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            
         }
     }
 }
