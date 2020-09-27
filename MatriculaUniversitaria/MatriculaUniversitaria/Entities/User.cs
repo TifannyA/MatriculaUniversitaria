c9236df936d52003160af1278d3ffe7662a0892d
JUAN DANIEL TREJOS PEREZ <jtrejosp@est.utn.ac.cr>
2020-09-27 14:54:36 -0600

    agregar clases sección administrador

---
+++ MatriculaUniversitaria/MatriculaUniversitaria/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatriculaUniversitaria.Entities;
+
+namespace MatriculaUniversitaria.Entities
+{
+    class User : Person
+    {
+        private int _user;
+        private int _pass;
+
+        public int user
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public int pass
+        {
+            get { return _pass; }
+            set { _pass = value; }
+        }
+
+
+
+        public User (int id, int dni, string name, string last, char sex, DateTime bornDate, string academyLvl, DateTime admisionDate, string admisionUser, string nationality, string state,int user,string pass) 
+            : base (id, dni, name, last, sex, bornDate, academyLvl, admisionDate, admisionUser, nationality, state)
+        {
+            this.user = _user;
+            this.pass = _pass;
+        }
+    }
+}
---
+++ MatriculaUniversitaria/MatriculaUniversitaria/Entities/career.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaUniversitaria.Entities
+{
+    class Career
+    {
+        private int _id;
+        private string _name;
+        private int _totalCredit;
+        private bool _state;
+        private DateTime _startDate;
+        private DateTime _endDate;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int totalCredit
+        {
+            get { return _totalCredit; }
+            set { _totalCredit = value; }
+        }
+
+        public bool state
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        public DateTime startDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        public DateTime endDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; }
+        }
+
+        public string toString() 
+        {
+            return "";
+        }
+    }
+}
