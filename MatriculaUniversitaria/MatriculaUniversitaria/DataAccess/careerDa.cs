1bd33124307d696db1d409454b364d74c2788b2a
JUAN DANIEL TREJOS PEREZ <jtrejosp@est.utn.ac.cr>
2020-09-27 15:51:41 -0600

    crear estructura del proyecto

---
+++ MatriculaUniversitaria/MatriculaUniversitaria/DataAccess/careerDa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatriculaUniversitaria.DataAccess
+{
+    class careerDa
+    {
+        //crear metodos para agregar en list
+
+        //crear metodos para editar en list
+
+        //crear metodo para eliminar en list
+
+        //crear metodo para enviar un string de la información en list
+    }
+}
---
+++ MatriculaUniversitaria/MatriculaUniversitaria/DataAccessObject/careerDao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatriculaUniversitaria.Entities;
+
+namespace MatriculaUniversitaria.DataAccessObject
+{
+    class careerDao
+    {
+        private List<Career> _careers;
+
+        public List<Career> careers
+        {
+            get { return _careers; }
+            set { _careers = value; }
+        }
+
+        public careerDao()
+        {
+            this._careers = new List<Career>(); ;
+        }
+    }
+}
