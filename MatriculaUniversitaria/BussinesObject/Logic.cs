dd00cee055a122effe86554a1b542f0511c098c1
JUAN DANIEL TREJOS PEREZ <jtrejosp@est.utn.ac.cr>
2020-10-03 20:54:58 -0600

    get y set de linkedlist

--- MatriculaUniversitaria/BussinesObject/Logic.cs
+++ MatriculaUniversitaria/BussinesObject/Logic.cs
@@ -53,11 +53,57 @@ namespace matriculaUniversitaria.BussinesObject
             _people.Find(e).Value=p;  
         }
 
-        public eliminarPersona(Person p) 
+        public void eliminarPersona(Person p) 
         {
             _people.Remove(p);
         }
 
+        public void agregarCarrera(Career c)
+        {
+            _career.AddLast(c);
 
+        }
+
+        public void editarCarrera(Career p, Career e)
+        {
+            _career.Find(e).Value = p;
+        }
+
+        public void eliminarCarrera(Career p)
+        {
+            _career.Remove(p);
+        }
+
+        public void agregarCurso(Course p)
+        {
+            _Course.AddLast(p);
+
+        }
+
+        public void editarCurso(Course p, Course e)
+        {
+            _Course.Find(e).Value = p;
+        }
+
+        public void eliminarCurso(Course p)
+        {
+            _Course.Remove(p);
+        }
+
+        public void agregarUsuario(Usuario p)
+        {
+            _users.AddLast(p);
+
+        }
+
+        public void editarUsuario(Usuario p, Usuario e)
+        {
+            _users.Find(e).Value = p;
+        }
+
+        public void eliminarUsuario(Usuario p)
+        {
+            _users.Remove(p);
+        }
     }
 }
