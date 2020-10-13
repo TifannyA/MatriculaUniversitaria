59cc8bd61d95bc6c959a157460c276b7e033a422
Tifanny Artavia Corella <alejandra.artaviacorella@gmail.com>
2020-10-13 17:44:40 -0600

    terminando login de usuarios

--- matriculaUniversitaria/Program.cs
+++ matriculaUniversitaria/Program.cs
@@ -18,7 +18,7 @@ namespace matriculaUniversitaria
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistrarUser());
+            Application.Run(new Form1());
         }
 
 
