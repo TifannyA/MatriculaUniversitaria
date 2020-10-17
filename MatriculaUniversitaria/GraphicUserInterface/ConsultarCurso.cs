dd56d6b0c13e064de22ed63f8fcb64c16f589e3d
Juanda96 <50851266+Juanda96@users.noreply.github.com>
2020-10-17 15:47:01 -0600

    terminar consultar curso

--- MatriculaUniversitaria/GraphicUserInterface/ConsultarCurso.cs
+++ MatriculaUniversitaria/GraphicUserInterface/ConsultarCurso.cs
@@ -16,16 +16,19 @@ namespace matriculaUniversitaria.GraphicUserInterface
     {
         LinkedList<Career> careers = new LinkedList<Career>();
         careerDA cda = new careerDA();
+        LinkedList<Course> courses = new LinkedList<Course>();
+        courseDA cuda = new courseDA();
         public ConsultarCurso()
         {
             careers = cda.readCareer();
             InitializeComponent();
             cargarCarrera();
+            courses = cuda.readCourse();
         }
 
         private void ConsultarCurso_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void cargarCarrera() {
@@ -53,12 +56,52 @@ namespace matriculaUniversitaria.GraphicUserInterface
 
         private void btnEditar_Click(object sender, EventArgs e)
         {
-
+            if (Lista.SelectedIndex < 0)
+            {
+                MessageBox.Show("Seleccione un curso para editar");
+            }
+            else 
+            {
+                EditarCurso ec = new EditarCurso(Lista.SelectedIndex);
+                ec.Show();
+            }
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
+            if (Lista.SelectedIndex < 0)
+            {
+                MessageBox.Show("Seleccione un curso para eliminar");
+            }
+            else
+            {
+                Course c = new Course();
+                c = courses.ElementAt(Lista.SelectedIndex);
+                DialogResult boton = MessageBox.Show("Desea eliminar a " + c.name + "?", "Alerta", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (boton == DialogResult.OK)
+                {
+                    courses.Remove(c);
+                    cuda.writeCourse(courses);
+                }
+            }
+        }
 
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+                if (cmbCareer.SelectedIndex < 0)
+                {
+                    MessageBox.Show("Seleccione una carrera para ver los cursos");
+                }
+                else 
+                {
+                    foreach (var c in courses)
+                    {
+                    if (c.idCareer.Equals(careers.ElementAt(cmbCareer.SelectedIndex).id))
+                    {
+                        Lista.Items.Add(c.printCourse());
+                    }
+                    }
+                }
         }
     }
 }
---
+++ MatriculaUniversitaria/GraphicUserInterface/EditarCurso.cs
@@ -0,0 +1,60 @@
+﻿using matriculaUniversitaria.DataAccess;
+using MatriculaUniversitaria.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matriculaUniversitaria.GraphicUserInterface
+{
+    public partial class EditarCurso : Form
+    {
+        courseDA cda = new courseDA();
+        LinkedList<Course> cursos = new LinkedList<Course>();
+        Course curso;
+        public EditarCurso(int num)
+        {
+            InitializeComponent();
+            cursos = cda.readCourse();
+            this.curso = cursos.ElementAt(num);
+            lblPreCodigo.Text = curso.id;
+            lblCodigo.Text = curso.idCareer;
+            txtCodigo.Text = curso.id.Split('-')[1].Trim();
+            txtNombre.Text = curso.name;
+            txtPrecio.Text = ""+curso.price;
+            txtCreditos.Text = "" + curso.credits;
+            txtCosto.Text = "" + curso.totalCost;
+
+        }
+
+        private void EditarCurso_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text.Equals("") && txtNombre.Text.Equals("") && txtCreditos.Text.Equals("") && txtPrecio.Text.Equals("") && txtCosto.Text.Equals(""))
+            {
+                MessageBox.Show("Error: Datos incompletos");
+            }
+            else
+            {
+                Course c = new Course(lblPreCodigo.Text, txtNombre.Text, int.Parse(txtCreditos.Text),curso.idCareer, int.Parse(txtPrecio.Text), int.Parse(txtCosto.Text));
+                cursos.Find(curso).Value = c;
+                cda.writeCourse(cursos);
+            }
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            lblPreCodigo.Text = curso.idCareer + " - " + txtCodigo.Text;
+        }
+    }
+}
